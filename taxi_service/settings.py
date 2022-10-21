520a5be0a8fb0f012a2b7998d7a3a2134e2cccfe
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-10-21 20:02:52 +0300

    release 1.1

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -145,8 +145,8 @@ STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = "media/"
+MEDIA_URL = "/"
+MEDIA_ROOT = "/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
