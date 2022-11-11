ee50ea055162ecfbfdafbbe1a9d483471b8df555
Volodymyr Vereshchak <diakonx1985@gmail.com>
2022-11-11 19:06:35 +0200

    fix DJANGO_SECRET_KEY

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = "django-insecure-8ovil3xu6=eaoqd#-#&ricv159p0pypoh5_lgm*)-dfcjqe=yc"
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-8ovil3xu6=eaoqd#-#&ricv159p0pypoh5_lgm*)-dfcjqe=yc")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 # DEBUG = True
